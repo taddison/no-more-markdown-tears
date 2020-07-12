cf120b563c3fdc9f62ae3055e2bd9be175d36003
Tim Addison <git@tjaddison.com>
2020-07-12 16:17:00 -0400

    check for author

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
-const { resolve } = require('path');
-
-const fs = require('fs').promises;
+const { resolve } = require("path");
+const fs = require("fs").promises;
+const matter = require("gray-matter");
 
 const getFiles = async (path) => {
-  const directoryEntries = await fs.readdir(path, { withFileTypes: true});
-  
-  const files = await Promise.all(directoryEntries.map(directoryEntry => {
-    const resolvedPath = resolve(path, directoryEntry.name);
-    return directoryEntry.isDirectory() ? getFiles(resolvedPath) : resolvedPath
-  }));
-  
+  const directoryEntries = await fs.readdir(path, { withFileTypes: true });
+
+  const files = await Promise.all(
+    directoryEntries.map((directoryEntry) => {
+      const resolvedPath = resolve(path, directoryEntry.name);
+      return directoryEntry.isDirectory()
+        ? getFiles(resolvedPath)
+        : resolvedPath;
+    })
+  );
+
   return files.flat();
-}
+};
 
 const getFilesToCheck = async (path, extensions) => {
   const files = await getFiles(path);
-  const filtered = files.filter(file => {
-    return extensions.some(extension => file.endsWith(extension));
-  })
+  const filtered = files.filter((file) => {
+    return extensions.some((extension) => file.endsWith(extension));
+  });
 
   return filtered;
+};
+
+const getFileContents = async (path) => {
+  return await fs.readFile(path, 'utf-8');
+};
+
+const checkFrontMatter = (filePath, data) => {
+  const parsed = matter(data);
+  if(Object.entries(parsed.data).length === 0) {
+    console.warn(`WARN: File ${filePath} has no frontmatter`);
+    return;
+  }
+  
+  if(!parsed.data.title) {
+    console.warn(`WARN: File ${filePath} has no title`);
+  }
 }
 
-const sourcePath = "c:\\src\\blog\\content";
+const sourcePath = "c:\\src\\blog\\content\\blog";
 const extensions = ["md"];
 (async () => {
   const filesToCheck = await getFilesToCheck(sourcePath, extensions);
-  console.log(filesToCheck)
-}
-)();
+  for (const file of filesToCheck) {
+    const fileContents = await getFileContents(file);
+    checkFrontMatter(file, fileContents);
+  }
+})();
